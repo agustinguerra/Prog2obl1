99f5133585c850ec6530f4c05eb9109bae8d6866
agustinguerra <agustin.guerraguerra8@gmail.com>
2017-04-23 13:59:02 +0200

    Getters and setters ficha y tablero

--- Prog2/src/prog2/Ficha.java
+++ Prog2/src/prog2/Ficha.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package prog2;
 
-/**
- *
- * @author Agustin
- */
+
 public class Ficha {
+    private int color;
+
+
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RED = "\u001B[31m";
+
+    
+    public Ficha() {
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public String pintar(int color) {
+        String ficha = "";
+        switch (color) {
+            case 2:
+                ficha = ANSI_BLUE + "Ê˜";
+                break;
+            case 1:
+                ficha = ANSI_RED + "Ê˜";
+                break;
+            case 0:
+                ficha = " ";
+                break;
+            default:
+                break;
+        }
+        return ficha;
+    }
+    
+//    @Override
+//    public int compareTo(Ficha f) {
+//        return (this.getColor() - f.getColor());
+//    }
+    
     
 }
--- Prog2/src/prog2/Tablero.java
+++ Prog2/src/prog2/Tablero.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package prog2;
 
-/**
- *
- * @author Agustin
- */
+
 public class Tablero {
+
+    private int[][] matriz;
+    private Ficha ficha;
+
+    public Tablero() {
+    }
+
+    public int[][] getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(int[][] matriz) {
+        this.matriz = matriz;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) {
+        this.ficha = ficha;
+    }
     
 }
