2d2a453c5c69b47c4910e18fcbeed1f7bf57ae93
juanchibruzzone <juanchibuzzone@gmail.com>
2017-04-25 20:48:07 -0300

    se agregaron comentarios
    comentarios que hace cada cosa

--- Prog2/src/prog2/Persona.java
+++ Prog2/src/prog2/Persona.java
@@ -2,9 +2,11 @@ package prog2;
 
 public abstract class Persona {
 
+    //VARIABLES PRIVADAS DE LA CLASE PERSONA QUE ES EL PADRE DE JUGADOR
     private String nombre;
     private int edad;
 
+    //METODOS DE ACCESO Y MODIFICACION DE LA CLASE PERSONA
     public String getNombre() {
         return nombre;
     }
@@ -21,15 +23,18 @@ public abstract class Persona {
         this.edad = edad;
     }
 
+    //CONSTRUCTOR CON PARAMETROS DE LA CLASE PERSONA
     public Persona(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
     }
 
+    //CONSTRUCTOR VACIO DE LA CLASE PERSONA.
     public Persona() {
 
     }
 
+    //METODO TO STRING DE LA CLASE PERSONA.
     @Override
     public String toString() {
         return "Persona{" + "nombre=" + nombre + ", edad=" + edad + '}';
