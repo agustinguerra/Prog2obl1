64459e69416853c76dfaeaa95897fe0532122966
juanchibruzzone <juanchibuzzone@gmail.com>
2017-04-25 21:28:46 -0300

    EN CLASE MAIN AGREGE METODOS
    estos metodos son scanners completos de datos INT o STRING

--- Obligatorio1/src/Obligatorio1/Program.java
+++ Obligatorio1/src/Obligatorio1/Program.java
@@ -1,8 +1,43 @@
 package Obligatorio1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Program {
     
     public static void main(String[] args) {
         
+    }   
+
+    //METODO ESTATICO QUE PIDE UN DATO DE TIPO STRING Y LO DEVUELVE.
+    public static String pidoDatoString(String msg) {
+        //Este metodo pide un dato de tipo String. 
+        System.out.println(msg);
+        String x = (new Scanner(System.in)).nextLine();
+        while (x.length() == 0) {
+            System.out.println(msg);
+            x = (new Scanner(System.in)).nextLine();
+        }
+        return x;
+    }
+
+    //METODO ESTATICO QUE PIDE UN DATO DE TIPO INT Y LO DEVUELVE. MANEJA EXCEPCIONES.
+    public static int pidoDatoIntPositivo(String msg, int minValue, int maxValue, int check1) {
+        int x = 0;
+        boolean cond = true;
+        while (cond) {
+            try {
+                System.out.println(msg);
+                x = (new Scanner(System.in)).nextInt();
+                if (x > minValue && x < maxValue && x != check1) {
+                    cond = false;
+                } else {
+                    System.out.println("Debe ser mayor que " + minValue + " y menor que " + maxValue + " y no puede ser " + check1);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debe ingresar un numero. ");
+            }
+        }
+        return x;
     }
 }
