c5358d85f9ce7e6bb154998a90ce1cac4c4332b1
juanchibruzzone <juanchibuzzone@gmail.com>
2017-04-25 21:09:20 -0300

    done
    done

--- Obligatorio1/src/Obligatorio1/Jugador.java
+++ Obligatorio1/src/Obligatorio1/Jugador.java
@@ -22,6 +22,6 @@ public class Jugador extends Persona {
 
     @Override
     public String toString() {
-        return "Jugador{" + "alias=" + alias + '}';
+        return "Jugador{" + "alias=" + alias + '}' + super.toString();
     }
 }
