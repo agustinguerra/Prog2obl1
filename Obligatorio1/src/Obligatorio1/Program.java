2fa72d417a85951f5ad7cff3d44c9882db0a1476
juanchibruzzone <juanchibuzzone@gmail.com>
2017-04-25 21:22:57 -0300

    elimine una prueba de main (program)
    d

--- Obligatorio1/src/Obligatorio1/Program.java
+++ Obligatorio1/src/Obligatorio1/Program.java
@@ -3,10 +3,6 @@ package Obligatorio1;
 public class Program {
     
     public static void main(String[] args) {
-        Jugador s1 = new Jugador();
-        s1.setEdad(23);
-        s1.setAlias("lucio");
-        s1.setNombre("nombre");
-        System.out.println(s1);
+        
     }
 }
